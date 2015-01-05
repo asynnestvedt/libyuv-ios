ac19b16c7a8f718d0c93fce2ba1eff3547e9ad5f
Alan Synnestvedt <asynnestvedt@gmail.com>
2015-01-04 21:48:08 -0500

    reduced to one static lib

--- lib/make-universal.sh
+++ lib/make-universal.sh
@@ -1,5 +1,4 @@
-rm ./libyuv.a 2>/dev/null
-lipo -create -arch armv7 ./armv7/libyuv.a -arch arm64 ./arm64/libyuv.a -output ./libyuv.a
-
-rm ./libyuv_neon.a 2>/dev/null
-lipo -create -arch armv7 ./armv7/libyuv_neon.a -arch arm64 ./arm64/libyuv_neon.a -output ./libyuv_neon.a
+rm ./libyuv.a ./armv7/libyuv_merged.a ./arm64/libyuv_merged.a 2>/dev/null
+libtool -static ./armv7/libyuv.a ./armv7/libyuv_neon.a -o ./armv7/libyuv_merged.a
+libtool -static ./arm64/libyuv.a ./arm64/libyuv_neon.a -o ./arm64/libyuv_merged.a
+lipo -create -arch armv7 ./armv7/libyuv_merged.a -arch arm64 ./arm64/libyuv_merged.a -output ./libyuv.a
